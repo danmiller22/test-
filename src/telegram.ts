8a00a037a771ef03625c929744133d4f7c5fd29f
Mueller <ajdarn3@gmail.com>
2025-10-15 15:18:47 -0500

    Update telegram.ts

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,4 +1,4 @@
-import { Bot } from "https://deno.land/x/grammy@v1.25.4/mod.ts";
+import { Bot } from "https://deno.land/x/grammy@v1.25.1/mod.ts";
 
 const token = Deno.env.get("TELEGRAM_TOKEN");
 if (!token) throw new Error("Missing TELEGRAM_TOKEN");
